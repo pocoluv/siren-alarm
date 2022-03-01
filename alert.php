19429e7405ce0ec4c343c50cf08c1918cb395047
Max Eve <deadmoz5er@gmail.com>
2022-03-01 20:32:12 +0200

    Fix output

--- alert.php
+++ alert.php
@@ -3,10 +3,10 @@ define('FILE', 'alertStatus');
 
 $alertStatus = file_get_contents(FILE);
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo $alertStatus;
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     file_put_contents(FILE, !$alertStatus);
+    $alertStatus = !$alertStatus;
 }
 
+echo $alertStatus;
 ?>
