d7f2061f1c470d3b7a62d1479fc31608406324f3
Max Eve <deadmoz5er@gmail.com>
2022-03-01 20:37:20 +0200

    wtf fix

--- alert.php
+++ alert.php
@@ -5,15 +5,8 @@ $alertStatus = file_get_contents(FILE);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     file_put_contents(FILE, !$alertStatus);
-<<<<<<< HEAD
     $alertStatus = !$alertStatus;
 }
 
 echo $alertStatus;
 ?>
-=======
-    echo !$alertStatus;
-}
-
-?>
->>>>>>> e3388aae4061b5c3d6bbbb895c6fc5b912ecd42a
